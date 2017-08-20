edd1031b57d6a8a36543212a4e6494be2b851235
reinvantveer <info@bureaudigitaalerfgoed.nl>
2017-08-20 20:46:18 +0200

    Create snapshots form, convert submitted subtitles to .srt

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ Create new, burned in permanent subtitles for a movie fragment
 - [X] Create an empty express layout
 - [X] Select a movie fragment
 - [X] Shorten movie fragment
-- [ ] Create snapshots
-- [ ] Create a form with 8 (?) snapshots
-- [ ] Create an API endpoint for POSTing new subtitle phrases
-- [ ] Convert submitted subtitle phrases to .srt format
+- [X] Create snapshots
+- [X] Create a form with 8 (?) snapshots
+- [X] Create an API endpoint for POSTing new subtitle phrases
+- [X] Convert submitted subtitle phrases to .srt format
+- [ ] Store submitted subtitle phrases to srt file
 - [ ] Call movie conversion program with .srt and movie parameters
+- [ ] Synchronize subtitles with movie
 - [ ] Return the newly converted movie as binary
  
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const subTransformer = require('./lib/subtitleTransformer').transformSubs;
 
-app.set('view engine', 'pug')
-app.get('/', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
+// Initialize the server-side app
+const app = express();
+app.set('view engine', 'pug');
+app.use('/stills', express.static('stills'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+// Paths
+app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello there!' }));
+app.post('/sub', (req, res) => res.send(subTransformer(req.body)));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
